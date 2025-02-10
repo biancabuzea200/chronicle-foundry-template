d7e23b3c88f1481204497ee33d38e149c3e4807d
biancabuzea200 <biancabuzea200@gmail.com>
2025-02-11 00:57:36 +0100

    also install yarn

--- start.sh
+++ start.sh
@@ -4,5 +4,9 @@
  sudo apt install curl --yes
  sudo apt install git --yes
  curl -L https://foundry.paradigm.xyz | bash
- source /root/.bashrc
+ . /root/.bashrc
  foundryup
+ sudo apt install nodejs --yes
+ sudo apt install npm --yes
+ npm install --global yarn
+
