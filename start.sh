7db5f73d6e9c0f29c41a430c573749ad8ed988cf
biancabuzea200 <biancabuzea200@gmail.com>
2025-02-11 00:35:18 +0100

    edit

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
- yes | apt update &&  apt upgrade
- apt-get install sudo -y
- yes | sudo apt install curl
- yes | sudo apt install git
+ apt update &&  apt upgrade --yes
+ apt-get install sudo --yes
+ sudo apt install curl --yes
+ sudo apt install git --yes
  curl -L https://foundry.paradigm.xyz | bash
  source /root/.bashrc
  foundryup
